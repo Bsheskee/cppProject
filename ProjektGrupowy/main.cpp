9ea6bd853bf9de579420805cfcc166e7bc02a54d
bsheskee <bialobrzewski.business@gmail.com>
2025-06-01 16:12:32 +0200

    Initial commit.

--- ProjektGrupowy/main.cpp
+++ ProjektGrupowy/main.cpp
@@ -2,13 +2,82 @@
 //  main.cpp
 //  ProjektGrupowy
 //
-//  Created by Bartłomiej Białobrzewski on 12/05/2025.
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream> //- konwersja string na int (istringstream)
+#include <iomanip> //- formatowanie wyjścia
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+double obliczSrednia(const int oceny[], int rozmiar);
+
+struct STUDENT {
+    string imieNazwisko;
+    int oceny[6];
+    double srednia;
+    string ocenaKoncowa;
+};
+
+int main() {
+    ifstream plik("dane.txt");
+    vector<STUDENT> studenci;
+    
+    if (!plik.is_open()) {
+        cerr << "Nie mozna otworzyc pliku!" << endl;
+        return 1;
+    }
+    
+    string linia;
+    while (getline(plik, linia)) {
+        STUDENT student;
+        student.imieNazwisko = linia;
+        
+        if (getline(plik, linia)) {
+            istringstream iss(linia); // do odczytu ocen z pliku jako int zamiast string <sstream>
+            for (int i = 0; i < 6; i++) {
+                iss >> student.oceny[i]; // pobieramy oceny jako inty z pliku dla każdego studenta
+            }
+            student.srednia = obliczSrednia(student.oceny, 6);
+            studenci.push_back(student);
+        }
+    }
+    plik.close();
+
+    // Wyświetlenie wyników
+    cout << "Dane studentów:" << endl;
+    cout << "-------------------------------------------------------" << endl;
+    cout << setw(20) << left << "Imię i nazwisko"
+    << setw(25) << "Oceny"
+    << setw(10) << "Średnia"
+    << "Ocena końcowa" << endl;
+    cout << "-------------------------------------------------------" << endl;
+    
+    for (const auto& student : studenci) {
+        cout << setw(20) << left << student.imieNazwisko << " ";
+        
+        // Wyświetlanie ocen
+        cout << "[";
+        for (int i = 0; i < 6; ++i) {
+            cout << student.oceny[i];
+            if (i < 5) cout << ", ";
+        }
+        cout << "] ";
+        
+        // Wyświetlanie średniej i oceny końcowej
+        cout << setw(9) << fixed << setprecision(2) << student.srednia << " ";
+        cout << endl;
+//        cout << student.ocenaKoncowa << endl;
+    }
+    
     return 0;
+}
+
+double obliczSrednia(const int oceny[], int rozmiar) {
+    double suma = 0;
+    for (int i = 0; i < rozmiar; i++) {
+        suma += oceny[i];
+    }
+    return suma / rozmiar;
 }
